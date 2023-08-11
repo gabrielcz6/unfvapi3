40b12ea9864d17481d9218f6d1abe5588409d0bf
Gabriel <gabrielcanepamercado@gmail.com>
2023-08-11 18:53:11 -0500

    add permissions

--- django_project/settings.py
+++ django_project/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = { # new
 "DEFAULT_PERMISSION_CLASSES": [
-"rest_framework.permissions.AllowAny",
+#"rest_framework.permissions.AllowAny",
+"rest_framework.permissions.IsAuthenticated", 
 ],
 }
 
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,16 @@
 # posts/views.py
-from rest_framework import generics
+from rest_framework import generics, permissions # new
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly # new
+
 
 
 class PostList(generics.ListCreateAPIView):
    queryset = Post.objects.all()
+   permission_classes = (IsAuthorOrReadOnly,) # new
    serializer_class = PostSerializer
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+   permission_classes = (IsAuthorOrReadOnly,) # new
    queryset = Post.objects.all()
    serializer_class = PostSerializer

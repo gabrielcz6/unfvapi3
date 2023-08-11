f71f7e930b29ea33b83bf7b8aba440b9f7af0a18
Gabriel <gabrielcanepamercado@gmail.com>
2023-08-11 18:49:38 -0500

    Blog API setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+AUTH_USER_MODEL = "accounts.CustomUser" # new
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -37,17 +37,36 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework", # new
+    "corsheaders", # new
+    "accounts.apps.AccountsConfig", # new
+    "posts.apps.PostsConfig", # new
 ]
+
+REST_FRAMEWORK = { # new
+"DEFAULT_PERMISSION_CLASSES": [
+"rest_framework.permissions.AllowAny",
+],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware", # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+"http://localhost:3000",
+"http://localhost:8000",
+)
+
+# django_project/settings.py
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"] # new
 
 ROOT_URLCONF = 'django_project.urls'
 
